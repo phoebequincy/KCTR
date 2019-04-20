02d7eae6da18f7c2db23abcbdf574b15675c42e7
Phoebe Quincy <phoebequincy@gmail.com>
2019-04-19 20:47:54 -0600

    Setting up components, installing browser-router.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../stylesheets/App.css';
+import { Route } from 'react-router-dom'
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import Overview from './components/Overview';
+import Ropes from './components/Ropes';
+
+const API = process.env.REACT_APP_API
 
 class App extends Component {
+  state = {
+    ropeinst: [],
+  }
+
+  async componentDidMount() {
+    const response = await fetch(`${API}/ropeinst`)
+    const json = await response.json()
+    this.setState({...this.state, ropeinst: json})
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <div>
+          <div>
+            <NavBar />
+              <Route path="/" exact component={Home} />
+
+              <Route path="/Ropes"
+                component={() =>
+                  <Ropes
+                  ropesinst={this.state.ropesinst}
+                  />}
+              />
+
+              <Route path="/Overview"
+                component={()=>
+                  <Overview
+
+                  />}
+              />
+
+              <Route path="/Home"
+                component={()=>
+                  <Home
+
+                  />}
+              />
+          </div>
+        </div>
+    )
   }
 }
 
